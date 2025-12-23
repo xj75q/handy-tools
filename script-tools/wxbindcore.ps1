c835cb621155279ae5984ea5c87058ee9faa9a78
alecqie <alecqie@163.com>
2025-12-23 17:58:11 +0800

    wxbindcore

--- script-tools/wxbindcore.ps1
+++ script-tools/wxbindcore.ps1
@@ -1,22 +1,18 @@
-$wxid=(Get-Process -Name "Wechat" | Select-Object).Id
-$process = Get-Process -Id $wxid
-$process.ProcessorAffinity = [System.IntPtr]128 #第7个核心
+$wxid=(Get-Process -Name "Weixin" | Select-Object).Id
+foreach ($element in $wxid) {
+  $process = Get-Process -Id $element
 
-$wxocrid=(Get-Process -Name "WechatOCR" | Select-Object).Id
-$process = Get-Process -Id $wxocrid
-$process.ProcessorAffinity = 32 #第5个核心
-
-$wxplyid=(Get-Process -Name "WechatPlayer" | Select-Object).Id
-$process = Get-Process -Id $wxplyid
-$process.ProcessorAffinity = 32 #第5个核心
-
-$wxu3did=(Get-Process -Name "WeChatUtility" | Select-Object).Id
-$process = Get-Process -Id $wxu3did
-$process.ProcessorAffinity = 32 #第5个核心
-
-
+  $process.ProcessorAffinity = [System.IntPtr]128 #第7个核心
+  $process.MaxWorkingSet = 90mb
+}
+$wxocrid=(Get-Process -Name "WeChatAppEx" | Select-Object).Id
+foreach ($element in $wxocrid) {
+  $process = Get-Process -Id $element
+  $process.ProcessorAffinity = [System.IntPtr]64 #第6个核心
+  $process.MaxWorkingSet = 40mb
+}
 $edgeid=(Get-Process -Name "msedge*" | Select-Object).Id
 foreach ($element in $edgeid) {
-  $process = Get-Process -Id $element 
+  $process = Get-Process -Id $element
   $process.ProcessorAffinity = 0x0009 #绑定0和3个。
 }
